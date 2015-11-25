6506abfa880867156bab7417bc7b718742ab82a7
Vasilkova Polina <pvasilkova@yandex.ru>
2015-11-25 22:28:50 +0300

    /

--- punct3/convert1.h
+++ punct3/convert1.h
@@ -1,6 +1,6 @@
 #include "sdt.h"
-char convert_in_hex(uint16_t x);
-void print_in_hex1(uint16_t byte);
+char convert_in_hex(uint8_t x);
+void print_in_hex1(uint8_t byte);
 void print_in_hex(const void* data, size_t size);
-void print_in_binary1(uint16_t byte);
+void print_in_binary1(uint8_t byte);
 void print_in_binary(const void* data, size_t size);
